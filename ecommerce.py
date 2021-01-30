e6b4d902efb512fc2dbb3424695b8e31c502a217
shreyakodgi <67737111+shreyakodgi@users.noreply.github.com>
2021-01-30 22:53:38 +0530

    creates E-commerce Website

---
+++ ecommerce.py
@@ -0,0 +1,69 @@
+<! doctype html>
+
+<html lang="en">
+
+<head>
+
+<!-- Required meta tags -->
+
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS ->
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+
+integrity="sha384-MCw98/SFnGE8F3T3GXwEOngsv7Zt27NXFOCOApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="
+
+anonymous">
+
+<title>Hello, world!</title>
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-light bg-light">
+
+<a class="navbar-brand">Navbar</a> <form class="form-inline">
+
+<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+
+<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+
+</form>
+
+</nav>
+
+<form>
+
+<div class="form-group row">
+
+<label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+
+<i class="col-sm-10">
+
+rout type="email" class="form-control" Id="inputEmai 19" placeholder="Email">
+
+</div>
+
+<tir class"form-group row">
+
+<label for="inputPassword" class="col-sm-2 ol-for-label">Passwords > <ul class "col-11-10"> <input type="password" class="form-control" d="inputPasswords" placeholder="Password">
+
+<div class="form-group row">
+
+label for="inputUsername class="col-sm-2 col-form-label">Username / <div class="col-sm-10">
+
+<input type="text" class="form-control" id="inputUsernam" placeholder="username>
+
+</div>
+
+</div class="form-group row">
+
+<div class"col-sm-10"> <button type="submit" class="btn btn-primary"Sign Up</utot </div>
+
+</div>
+
+</forms>
